c36dedadb459933849276ce844c91148ebbca4ec
Dario Faniglione <Dario.Faniglione@bcu.ac.uk>
2014-04-02 21:12:31 +0100

    Create handlers.py

---
+++ bp_content/themes/default/plugins/test/handlers/handlers.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+    A test plugin that render its template
+    Routes are setup in plugins/test/routes/__init__.py
+"""
+# standard library imports
+
+
+from bp_includes.lib.basehandler import BaseHandler
+
+class EditorHandler(BaseHandler):
+    """
+    Handler for test
+    """
+
+    def get(self):
+        params = {
+            
+        }
+
+        return self.render_template('test.html', **params)
+    
